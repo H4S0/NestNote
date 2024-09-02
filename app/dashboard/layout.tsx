81fa378488fafe381fea67b920415a1105289d5a
H4S0 <hasanalic10@gmail.com>
2024-09-02 09:43:41 +0200

    updated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,26 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import DashboardItems from "../components/dashboard/DashboardItems";
+import { DollarSign, Globe, HeartOff, Home } from "lucide-react";
+import { ThemeToggle } from "../components/dashboard/ThemeToggle";
+
+export const navLinks = [
+  {
+    name: 'Dashboard',
+    href: '/dashboard',
+    icon: Home,
+  },
+  {
+    name: 'Sites',
+    href: '/dashboard/sites',
+    icon: Globe
+  },
+  {
+    name: 'Pricing',
+    href: '/dashboard/pricing',
+    icon: DollarSign
+  }
+]
 
 export default function DashboardLayout({children} : {children: ReactNode}){
   return(
@@ -8,10 +29,23 @@ export default function DashboardLayout({children} : {children: ReactNode}){
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href='/' className="flex item-center gap-2 font-semibold">
-            <h3 className="text-2xl">Blog <span className="text-primary">Haso</span></h3>
+              <h3 className="text-2xl">Blog <span className="text-primary">Haso</span></h3>
             </Link>
           </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 font-medium lg:px-4">
+              <DashboardItems/>
+            </nav>
+          </div>
         </div>
+      </div>
+
+      <div className="flex flex-col ">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <div className="ml-auto flex items-center gap-x-5">
+            <ThemeToggle/>
+          </div>
+        </header>
       </div>
     </section>
   )
