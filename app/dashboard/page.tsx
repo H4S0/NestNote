2c1491ab0b14edcff061238fec8da991647f4e0f
H4S0 <hasanalic10@gmail.com>
2024-09-02 08:28:48 +0200

    add auth and start creating sidebar

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboradIndexPage = () => {
+  return (
+    <div>hello dashboard</div>
+  )
+}
+
+export default DashboradIndexPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const {getUser} = getKindeServerSession()
+  const session = await getUser()
+ 
   return (
-    <h1>hello world</h1>
+    <div className="p-10">
+      <h1>hello</h1>
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ): (
+        <div>
+          <RegisterLink>
+        <Button>Register</Button>
+      </RegisterLink>
+      <LoginLink>
+        <Button>Login</Button>
+      </LoginLink>
+        </div>
+      )}
+    </div>
   );
 }
