62307f1c75b8c5c1971b027e3265e178dd13f4b9
H4S0 <hasanalic10@gmail.com>
2024-09-07 19:46:50 +0200

    article section

--- app/dashboard/sites/[siteId]/create/page.tsx
+++ app/dashboard/sites/[siteId]/create/page.tsx
@@ -1,5 +1,14 @@
 import { Button } from '@/components/ui/button';
-import { ArrowLeft } from 'lucide-react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { ArrowLeft, Atom, AtomIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export default function ArticleCreating({
@@ -17,6 +26,30 @@ export default function ArticleCreating({
         </Button>
         <h1 className="text-xl font-semibold">Create Article</h1>
       </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Article Details</CardTitle>
+          <CardDescription>
+            Lorem ipsum dolor sit amet consectetur.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label>Title</Label>
+              <Input placeholder="Blog" />
+            </div>
+
+            <div className="grid gap-2">
+              <Label>Slug</Label>
+              <Input placeholder="article slug" />
+              <Button className="w-fit" variant="secondary" type="button">
+                <AtomIcon className="mr-2" /> Generate Slug
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </>
   );
 }
