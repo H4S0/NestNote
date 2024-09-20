f9d3cfb71fbbef942c6e41666a85b3ac39764892
H4S0 <hasanalic10@gmail.com>
2024-09-20 19:59:39 +0200

    dashboard building

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { Calendar } from '@/components/ui/calendar';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 const DashboradIndexPage = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
   return (
-    <div>hello dashboard</div>
-  )
-}
+    <>
+      <div>
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex flex-row w-full gap-4">
+            <Card className="w-full ">
+              <CardHeader>Total Notebooks</CardHeader>
+              <CardContent>notebooks_number</CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader>Total Notebooks</CardHeader>
+              <CardContent>notebooks_number</CardContent>
+            </Card>
+          </div>
+          <div className="flex flex-row w-full gap-4">
+            <Card className="w-full">
+              <CardHeader>Total Notebooks</CardHeader>
+              <CardContent>notebooks_number</CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader>Total Notebooks</CardHeader>
+              <CardContent>notebooks_number</CardContent>
+            </Card>
+          </div>
+        </div>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border w-fit"
+        />
+      </div>
+    </>
+  );
+};
 
 export default DashboradIndexPage;
